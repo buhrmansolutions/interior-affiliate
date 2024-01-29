adcdba8d9af5c6862a8a79edb5b108b556980eee
Joel Buhrman <joel@buhrmansolutions.com>
2024-01-29 11:13:50 +0100

    Calculate number of rows

--- app/components/products/index.tsx
+++ app/components/products/index.tsx
@@ -5,7 +5,16 @@ import PageSelector from "../pageselector";
 import "./index.css";
 import { ShopContext } from "@/app/shop/ShopContext";
 
-const PRODUCTS_PER_PAGE = 16;
+const NUMBER_OF_ROWS = 2;
+const ITEM_WIDTH = 300;
+const GAP_WIDTH = 39;
+const GRID_WIDTH = window.innerWidth - 400 - 100 * 2;
+const SPACE_LEFT_FOR_ITEM = GRID_WIDTH % (ITEM_WIDTH + GAP_WIDTH) > ITEM_WIDTH;
+const ITEMS_PER_ROW =
+  Math.floor(GRID_WIDTH / (ITEM_WIDTH + GAP_WIDTH)) +
+  (SPACE_LEFT_FOR_ITEM ? 1 : 0);
+const PRODUCTS_PER_PAGE = ITEMS_PER_ROW * NUMBER_OF_ROWS;
+
 const Products = () => {
   const { products } = useContext(ShopContext);
 
